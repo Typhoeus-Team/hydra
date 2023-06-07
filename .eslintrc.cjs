b4600952e35c89428f7572f3fa4685d6920143ba
David Long <long.david@gmail.com>
2023-06-07 13:15:58 -0700

    Linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,17 +4,18 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: [
-    'standard-with-typescript',
-    'plugin:react/recommended'
-  ],
+  extends: ['standard-with-typescript', 'plugin:react/recommended'],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: ['tsconfig.json']
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ['react'],
   rules: {
+    // from https://stackoverflow.com/questions/42640636/react-must-be-in-scope-when-using-jsx-react-react-in-jsx-scope
+    // suppress errors for missing 'import React' in files
+    'react/react-in-jsx-scope': 'off',
+    // allow jsx syntax in js files (for next.js project)
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }]
   }
 }
--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,20 +1,20 @@
-import { useState } from "react"
+import { useState } from 'react'
 
-import { useAppSelector, useAppDispatch } from "../../app/hooks"
+import { useAppSelector, useAppDispatch } from '../../app/hooks'
 import {
   decrement,
   increment,
   incrementByAmount,
   incrementAsync,
   incrementIfOdd,
-  selectCount,
-} from "./counterSlice"
-import styles from "./Counter.module.css"
+  selectCount
+} from './counterSlice'
+import styles from './Counter.module.css'
 
-export function Counter() {
+export function Counter () {
   const count = useAppSelector(selectCount)
   const dispatch = useAppDispatch()
-  const [incrementAmount, setIncrementAmount] = useState("2")
+  const [incrementAmount, setIncrementAmount] = useState('2')
 
   const incrementValue = Number(incrementAmount) || 0
 
@@ -42,7 +42,9 @@ export function Counter() {
           className={styles.textbox}
           aria-label="Set increment amount"
           value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
+          onChange={(e) => {
+            setIncrementAmount(e.target.value)
+          }}
         />
         <button
           className={styles.button}
@@ -52,13 +54,15 @@ export function Counter() {
         </button>
         <button
           className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
+          onClick={async () => await dispatch(incrementAsync(incrementValue))}
         >
           Add Async
         </button>
         <button
           className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
+          onClick={() => {
+            dispatch(incrementIfOdd(incrementValue))
+          }}
         >
           Add If Odd
         </button>
