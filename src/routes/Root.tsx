d244705de878944d347cb8f5a92fc7aa2613e775
Austin Moore <amoore1337@gmail.com>
2023-06-10 08:59:41 -0400

    starting on quiz setup form

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -6,21 +6,23 @@ export function Root() {
 
   return (
     <Flex width="100vw" height="100vh" direction="column">
-      <Flex width="100%" p="4" bgColor="teal.300" gap="4">
-        <Heading as="h1" size="lg" color="teal.900">
+      <Flex width="100%" px="4" py="2" bgColor="blue.400" gap="4">
+        <Heading as="h1" size="lg" color="white" fontWeight="semibold">
           Hydra
         </Heading>
         {location.pathname === '/start-quiz' ? (
-          <Button as={Link} colorScheme="purple" to="/">
+          <Button as={Link} colorScheme="gray" to="/">
             Home
           </Button>
         ) : (
-          <Button as={Link} colorScheme="purple" to="/start-quiz">
+          <Button as={Link} colorScheme="gray" to="/start-quiz">
             Start Quiz
           </Button>
         )}
       </Flex>
-      <Outlet />
+      <Flex width="100%" flex="1" direction="column" overflow="auto" position="relative">
+        <Outlet />
+      </Flex>
     </Flex>
   );
 }
--- src/routes/StartQuiz/index.tsx
+++ src/routes/StartQuiz/index.tsx
@@ -1,3 +1,95 @@
+import {
+  Container,
+  FormControl,
+  type FormControlProps,
+  FormLabel,
+  Heading,
+  Select,
+  RadioGroup,
+  Radio,
+  HStack,
+  Flex,
+  Button,
+  Checkbox,
+  Stack,
+} from '@chakra-ui/react';
+import { type FormEventHandler } from 'react';
+
 export function StartQuiz() {
-  return <div>Start Quiz!</div>;
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    // TODO: Do stuff
+  };
+
+  return (
+    <Container maxW="100%" centerContent py="8">
+      <Container
+        centerContent
+        maxW="550px"
+        border="2px solid"
+        borderColor="blue.400"
+        rounded="md"
+        p="4"
+      >
+        <Heading as="h2" size="lg" color="blue.700" mb="4">
+          Quiz Setup
+        </Heading>
+
+        <form onSubmit={handleSubmit}>
+          <Flex direction="column" gap="4">
+            <InlineFormControl>
+              <FormLabel>Language:</FormLabel>
+              <Select>
+                <option>Spanish</option>
+                <option>French</option>
+                <option>German</option>
+              </Select>
+            </InlineFormControl>
+
+            <InlineFormControl>
+              <FormLabel>Type:</FormLabel>
+              <RadioGroup defaultValue="conjugation">
+                <HStack spacing="2">
+                  <Radio colorScheme="blue" value="conjugation">
+                    Conjugation
+                  </Radio>
+                  <Radio colorScheme="blue" value="declension">
+                    Declension
+                  </Radio>
+                </HStack>
+              </RadioGroup>
+            </InlineFormControl>
+
+            <InlineFormControl>
+              <FormLabel>Tense:</FormLabel>
+              <Stack spacing="2" direction={['column', 'row']}>
+                <Checkbox colorScheme="blue" defaultChecked value="present">
+                  Present
+                </Checkbox>
+                <Checkbox colorScheme="blue" defaultChecked value="preterite">
+                  Preterite
+                </Checkbox>
+                <Checkbox colorScheme="blue" defaultChecked value="future">
+                  Future
+                </Checkbox>
+                <Checkbox colorScheme="blue" defaultChecked value="conditional">
+                  Conditional
+                </Checkbox>
+              </Stack>
+            </InlineFormControl>
+          </Flex>
+
+          <Flex alignItems="center" justifyContent="center" mt="6">
+            <Button colorScheme="blue" type="submit">
+              Submit
+            </Button>
+          </Flex>
+        </form>
+      </Container>
+    </Container>
+  );
+}
+
+function InlineFormControl(props: FormControlProps) {
+  return <FormControl {...props} display="flex" alignItems="center" />;
 }
